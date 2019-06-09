c2d2e4c57c1a1bf37060a4ca0d672d9bcc1ac364
sveinjoe <sveinjoe@gmail.com>
2019-06-09 21:59:27 +0800

    第一次上传

---
+++ 亚马逊换ip工具/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace 亚马逊换ip工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxADSLAccount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxADSLPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTimerInterval = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxErrorStr = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxURI = new System.Windows.Forms.TextBox();
+            this.textBoxADSLName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxADSLAccount
+            // 
+            this.textBoxADSLAccount.Location = new System.Drawing.Point(90, 66);
+            this.textBoxADSLAccount.Name = "textBoxADSLAccount";
+            this.textBoxADSLAccount.Size = new System.Drawing.Size(203, 21);
+            this.textBoxADSLAccount.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "宽带账号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "宽带密码：";
+            // 
+            // textBoxADSLPass
+            // 
+            this.textBoxADSLPass.Location = new System.Drawing.Point(90, 101);
+            this.textBoxADSLPass.Name = "textBoxADSLPass";
+            this.textBoxADSLPass.Size = new System.Drawing.Size(203, 21);
+            this.textBoxADSLPass.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "间隔(毫秒)";
+            // 
+            // textBoxTimerInterval
+            // 
+            this.textBoxTimerInterval.Location = new System.Drawing.Point(90, 137);
+            this.textBoxTimerInterval.Name = "textBoxTimerInterval";
+            this.textBoxTimerInterval.Size = new System.Drawing.Size(203, 21);
+            this.textBoxTimerInterval.TabIndex = 4;
+            this.textBoxTimerInterval.Text = "3000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "检测字符：";
+            // 
+            // textBoxErrorStr
+            // 
+            this.textBoxErrorStr.Location = new System.Drawing.Point(90, 211);
+            this.textBoxErrorStr.Name = "textBoxErrorStr";
+            this.textBoxErrorStr.Size = new System.Drawing.Size(203, 21);
+            this.textBoxErrorStr.TabIndex = 6;
+            this.textBoxErrorStr.Text = "/errors/validateCaptcha";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开始检测";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "检测URL：";
+            // 
+            // textBoxURI
+            // 
+            this.textBoxURI.Location = new System.Drawing.Point(90, 176);
+            this.textBoxURI.Name = "textBoxURI";
+            this.textBoxURI.Size = new System.Drawing.Size(203, 21);
+            this.textBoxURI.TabIndex = 5;
+            this.textBoxURI.Text = "https://www.amazon.com/";
+            // 
+            // textBoxADSLName
+            // 
+            this.textBoxADSLName.Location = new System.Drawing.Point(90, 28);
+            this.textBoxADSLName.Name = "textBoxADSLName";
+            this.textBoxADSLName.Size = new System.Drawing.Size(203, 21);
+            this.textBoxADSLName.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "宽带名称：";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(21, 255);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 12);
+            this.labelResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 314);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxURI);
+            this.Controls.Add(this.textBoxErrorStr);
+            this.Controls.Add(this.textBoxTimerInterval);
+            this.Controls.Add(this.textBoxADSLPass);
+            this.Controls.Add(this.textBoxADSLName);
+            this.Controls.Add(this.textBoxADSLAccount);
+            this.Name = "Form1";
+            this.Text = "亚马逊验证码检测工具（换ip）";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxADSLAccount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxADSLPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTimerInterval;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxErrorStr;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxURI;
+        private System.Windows.Forms.TextBox textBoxADSLName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ 亚马逊换ip工具/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+using System.Net.Security;
+using System.Diagnostics;
+
+namespace 亚马逊换ip工具
+{
+    public partial class Form1 : Form
+    {
+        private static bool CheckValidationResult(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate, System.Security.Cryptography.X509Certificates.X509Chain chain, SslPolicyErrors errors)
+        {
+            return true; //总是接受     
+        }
+        private string filenameConfig = Application.StartupPath + "\\config.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //密码验证
+            string passwd = Interaction.InputBox("请输入密码", "密码", "请输入密码", -1, -1);
+            string datenow = DateTime.Now.ToString("yyyyMMddHH"); //获取当前时间到小时
+            if (passwd.Equals(string.Empty) == false)
+            {
+                string passstr = passwd + ":" + datenow;
+                string remotemd5 = GetHtml("http://www.sveinjoe.top/validate.php?pass=" + passwd, "");
+                string localmd5 = getMD5(passstr);
+                if (remotemd5.Equals(localmd5) == false)
+                {
+                    MessageBox.Show("验证成功！");
+                    this.Close(); //验证失败，关闭
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+            //验证成功
+            //读取配置文件
+            if (File.Exists(filenameConfig))
+            {
+                string[] arrConfig = File.ReadAllLines(filenameConfig);
+                if (arrConfig.Length == 6)
+                {
+                    textBoxADSLName.Text = arrConfig[0];
+                    textBoxADSLAccount.Text = arrConfig[1];
+                    textBoxADSLPass.Text = arrConfig[2];
+                    textBoxTimerInterval.Text = arrConfig[3];
+                    textBoxURI.Text = arrConfig[4];
+                    textBoxErrorStr.Text = arrConfig[5];
+                }
+            }
+        }
+        private string GetHtml(string url)
+        {
+            return GetHtml(url, "");
+        }
+
+        public string GetHtml(string url, string proxy)
+        {
+            string html = "";
+            try
+            {
+                HttpWebRequest request = null;
+                //HTTPSQ请求
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                request = WebRequest.Create(url) as HttpWebRequest;
+                if (proxy.Equals(string.Empty) == false)
+                {
+                    //检测是否使用代理
+                    WebProxy proxy1 = new WebProxy(proxy);
+                    request.Proxy = proxy1;
+                }
+                request.ProtocolVersion = HttpVersion.Version11;
+                request.Method = "GET";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) 1 Chrome/57.0.2987.133 Safari/537.36";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                Stream stream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                html = sr.ReadToEnd();
+            }
+
+            catch (System.Exception ex)
+            {
+
+                string errorMsg = ex.Message;
+                html = errorMsg;
+            }
+
+            return html;
+
+        }
+
+        private string getMD5(string str)
+        {
+            string returnstr = "";
+            byte[] result = Encoding.Default.GetBytes(str.Trim());
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] output = md5.ComputeHash(result);
+            returnstr = BitConverter.ToString(output).Replace("-", "");
+            return returnstr;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxADSLAccount.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("宽带欠费了吗？");
+                textBoxADSLAccount.BackColor = Color.Red;
+                return;
+            }
+            if (textBoxADSLPass.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("宽带欠费了吗？");
+                textBoxADSLPass.BackColor = Color.Red;
+                return;
+            }
+            if (textBoxADSLName.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("宽带名称的意思就是查看网络和共享中心，你用来拨号的那个宽带连接的图标的名字是什么？");
+                textBoxADSLPass.BackColor = Color.Red;
+                return;
+            }
+            //开始
+            int TimerInterval = 0;
+            int.TryParse(textBoxTimerInterval.Text, out TimerInterval); //设置线程
+            timer1.Interval = TimerInterval;
+            //保存当前设置
+            string[] arrConfig = new string[6];
+            arrConfig[0] = textBoxADSLName.Text;
+            arrConfig[1] += textBoxADSLAccount.Text;
+            arrConfig[2] += textBoxADSLPass.Text;
+            arrConfig[3] += textBoxTimerInterval.Text;
+            arrConfig[4] += textBoxURI.Text;
+            arrConfig[5] += textBoxErrorStr.Text;
+            File.WriteAllLines(filenameConfig, arrConfig); 
+            startCheck();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startCheck();
+        }
+
+        private void startCheck()
+        {
+            timer1.Start();
+            //拉取amazon.com的网页内容
+            labelResult.Text = "上次执行时间为：" + DateTime.Now.ToString();
+            string content = GetHtml(textBoxURI.Text);
+            if (content.IndexOf(textBoxErrorStr.Text) > 0)
+            {
+                ChangeIp();
+            }
+        }
+
+        /// <summary>
+        /// 开始拨号
+        /// </summary>
+        /// <param name="ADSL_Name">宽带连接名称</param>
+        /// <param name="ADSL_UserName">宽带连接用户名</param>
+        /// <param name="ADSL_PassWord">宽带连接密码</param>
+        private void ChangeIp()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+            sb.AppendLine("@echo off");
+            sb.AppendLine("set adslmingzi=" + textBoxADSLName.Text);
+            sb.AppendLine("set adslzhanghao=" + textBoxADSLAccount.Text);
+            sb.AppendLine("set adslmima=" + textBoxADSLPass.Text);
+            sb.AppendLine("@Rasdial %adslmingzi% /disconnect");
+            sb.AppendLine("ping 127.0.0.1 -n 2 > nul");
+            sb.AppendLine("Rasdial %adslmingzi% %adslzhanghao% %adslmima%");
+            sb.AppendLine("echo 连接中");
+            sb.AppendLine("ping 127.0.0.1 -n 4 > nul");
+            string temppath = Application.StartupPath + "\\temp.bat";
+            using (StreamWriter sw = new StreamWriter(temppath, false, Encoding.Default))
+            {
+                sw.Write(sb.ToString());
+            }
+            Process.Start(temppath);
+            int delay = 10;
+            System.Threading.Thread.Sleep(delay * 1000);
+            //File.Delete(temppath);
+        }
+    }
+}
